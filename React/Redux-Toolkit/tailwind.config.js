42272b6b8326fa0d5209ebd531f0e36736ee5be2
Samad Amir <samadaamir145@gmail.com>
2024-09-18 23:59:23 +0500

    next JS

--- React/Redux-Toolkit/tailwind.config.js
+++ React/Redux-Toolkit/tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // Ensure it points to all relevant source files
-  ],
+  content: ["*"],
   theme: {
     extend: {},
   },
